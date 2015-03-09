a2b139d2e1337eaba794cbbd5f3c8fcef61afb3c
jdpigeon <jdpigeon@hotmail.com>
2015-03-09 14:31:00 -0400

    begain training data analysis function and altered zone_other graphing so that APP and GFP sets are not grouped

--- watermaze_analysis.py
+++ watermaze_analysis.py
@@ -16,6 +16,7 @@ import numpy as np
 import glob #For use of wild cards in getting .ann files
 import matplotlib.pylab as plt
 import matplotlib.mlab as mlab
+import sys
 
 pd.set_option('display.precision',5)
 
@@ -26,9 +27,14 @@ def load_data(filename):
     Input: filename    
     Output: dataframe (df) object of data
     """
-    probe = pd.read_csv(filename, index_col="Animal")
+    if filename == 'trainingdata.csv':
+        probe = pd.read_csv(filename)
+        
+    else:
+        probe = pd.read_csv(filename, index_col="Animal")
     probe = probe.dropna(axis=1, how='all')
     probe = probe.dropna(axis=0, how='any')
+
     return probe
     
     
@@ -53,6 +59,84 @@ def label_groups(frame):
             j = j + 1
     
     return frame
+    
+def zone(frame):
+    """
+    Creates 2 plots showing 1.) average time in all zones and 2.) target zone vs average time spent in all other zones
+    
+    Input: probe data
+    Output: zone_all and zone_other plot 
+    """
+    gfpmice = frame[frame['Group']=='GFP']
+    appmice = frame[frame['Group']=='APP']
+    gfpmice = gfpmice[['Zone (% time)   Radius:   15.0','Unnamed: 41','Unnamed: 42','Unnamed: 43']]
+    appmice = appmice[['Zone (% time)   Radius:   15.0','Unnamed: 41','Unnamed: 42','Unnamed: 43']]
+    
+    #mean
+    avggfpzone = gfpmice.mean(axis=0)
+    avgappzone = appmice.mean(axis=0)
+    avggfpzone.name = "GFP"
+    avgappzone.name = "APP"
+    zone_all = pd.concat([avggfpzone, avgappzone], axis=1)
+    zone_all.index = ['1','T','3','4']
+    
+    #error
+    gfperr = gfpmice.apply(scipy.stats.sem, axis=0)
+    gfperr.name = "GFP"
+    apperr = appmice.apply(scipy.stats.sem, axis=0)
+    apperr.name = "APP"
+    err = pd.concat([gfperr,apperr], axis=1)
+    err.index = ['1','T','3','4']
+
+    zoneplot(zone_all,err)
+    
+    #mean
+    avgothergfp = avggfpzone[['Zone (% time)   Radius:   15.0','Unnamed: 42','Unnamed: 43']].mean(axis=0)
+    avgotherapp = avgappzone[['Zone (% time)   Radius:   15.0','Unnamed: 42','Unnamed: 43']].mean(axis=0)
+    zone_other = pd.concat([avggfpzone[1:3],avgappzone[1:3]],axis=0)
+    zone_other[1] = avgothergfp
+    zone_other[3] = avgotherapp
+    zone_other.index = ['T','O','T','O']
+    
+    #error
+    avggfperr = gfperr[['Zone (% time)   Radius:   15.0','Unnamed: 42','Unnamed: 43']].mean(axis=0)
+    avgapperr = apperr[['Zone (% time)   Radius:   15.0','Unnamed: 42','Unnamed: 43']].mean(axis=0)
+    othererr = pd.concat([gfperr[1:3],apperr[1:3]], axis=0)
+    othererr[1] = avggfperr
+    othererr[3] = avgapperr
+    othererr.index = ['T','O','T','O']
+    
+    otherplot(zone_other,othererr)
+
+def quadrant(frame):
+    """
+    Creates 1 plot showing average time in all quadrants
+    
+    Input: probe data
+    Output: quadrant plot
+    """
+    gfpmice = frame[frame['Group']=='GFP']
+    appmice = frame[frame['Group']=='APP']
+    gfpmice = gfpmice[['Quadrant time (%)','Unnamed: 29','Unnamed: 30','Unnamed: 31']].astype(float)
+    appmice = appmice[['Quadrant time (%)','Unnamed: 29','Unnamed: 30','Unnamed: 31']].astype(float)
+    
+    #error
+    gfperr = gfpmice.apply(scipy.stats.sem, axis=0)
+    gfperr.name = "GFP"
+    apperr = appmice.apply(scipy.stats.sem, axis=0)
+    apperr.name = "APP"
+    err = pd.concat([gfperr,apperr], axis=1)
+    err.index = ['R','T','L','O']
+    
+    #mean
+    avggfpzone = gfpmice.mean(axis=0)
+    avgappzone = appmice.mean(axis=0)
+    avggfpzone.name = "GFP"
+    avgappzone.name = "APP"
+    zonestats = pd.concat([avggfpzone, avgappzone], axis=1)
+    zonestats.index = ['R','T','L','O']
+    
+    quadplot(zonestats,err)
     
 ##########Graphing Functions##############
     
@@ -86,7 +170,19 @@ def zoneplot(frame, err):
     plt.xlabel('Zone')
     locs, labels = plt.xticks()
     plt.setp(labels,rotation=0)
+    plt.show()
     
+def otherplot(frame, err):
+    """
+    produces a bar graph where T and O indices have distinct columns for all groups (ie. GFP, APP)
+    """
+    frame.plot(kind='bar', yerr=err, color = ['blue','blue','green','green'])
+    plt.title('Time spent in Zone')
+    plt.ylabel('Time (sec)')
+    plt.xlabel('Zone')
+    locs, labels = plt.xticks()
+    plt.setp(labels,rotation=0)
+    plt.show()
     
 def quadplot(frame, err):
     """
@@ -108,92 +204,42 @@ def confidence_interval(data, confidence=0.95):
 
 
     """
-    
-
 To do:
-1. write exclude mouse function
-2. Change position of bars in zoneplot so that manipulation groups are seperate, with their distinct T O bars
-3. x
+4. write advanced exclude mouse function
+2. Finish up training data graphing function
+3. Alter zone_other graphing so that bars are grouped together
 """
 ##########################  
 
 if __name__ == "__main__":
     
     probe = load_data('C1C2probe.csv')
-    
-    grouped = label_groups(probe)
-    
-    gfpmice = probe[probe['Group']=='GFP']
-    appmice = probe[probe['Group']=='APP']
-    
-    ####ZONe
     """
-    gfpmice = gfpmice[['Zone (% time)   Radius:   15.0','Unnamed: 41','Unnamed: 42','Unnamed: 43']]
-    appmice = appmice[['Zone (% time)   Radius:   15.0','Unnamed: 41','Unnamed: 42','Unnamed: 43']]
+    trainingdata = load_data('trainingdata.csv')
+    trainingdata = trainingdata[['Animal','Date','Trial duration', 'Distance travelled (cm)', 'Average speed','% time near walls']]
+    grouped = trainingdata.groupby(['Animal','Date']).mean()
+    grouped = grouped.reset_index()
+    appmice = ['m3','m4','m7','m8']
+    gfpmice = ['m1','m2','m5','m6']
+    app = grouped[grouped['Animal'].isin(appmice)]
+    app.name=('APP')
+    gfp = grouped[grouped['Animal'].isin(gfpmice)]
+    gfp.name=('GFP')
+    app = app.groupby('Date').mean()
+    gfp = gfp.groupby('Date').mean()
+    
+    
+    appduration = app['Trial duration']
+    appduration.name=('APP')
+    gfpduration = gfp['Trial duration']
+    gfpduration.name=('GFP')
+    duration = pd.concat([gfpduration,appduration],axis=1)
+    """
+    label_groups(probe)
+    
     #gfpmice = gfpmice.drop('m2')
     
-    
-    #error
-    gfperr = gfpmice.apply(scipy.stats.sem, axis=0)
-    gfperr.name = "GFP"
-    apperr = appmice.apply(scipy.stats.sem, axis=0)
-    apperr.name = "APP"
-    err = pd.concat([gfperr,apperr], axis=1)
-    err.index = ['1','T','3','4']
-    #mean
-    avggfpzone = gfpmice.mean(axis=0)
-    avgappzone = appmice.mean(axis=0)
-    avggfpzone.name = "GFP"
-    avgappzone.name = "APP"
-    zonestats = pd.concat([avggfpzone, avgappzone], axis=1)
-    zonestats.index = ['1','T','3','4']
-    
-    zoneplot(zonestats,err)
-    
-    #mean
-    avgothergfp = avggfpzone[['Zone (% time)   Radius:   15.0','Unnamed: 42','Unnamed: 43']].mean(axis=0)
-    avgotherapp = avgappzone[['Zone (% time)   Radius:   15.0','Unnamed: 42','Unnamed: 43']].mean(axis=0)
-    otherstats = zonestats[1:3]
-    otherstats['GFP'][1] = avgothergfp
-    otherstats['APP'][1] = avgotherapp
-    otherstats.index = ['T','O']
-    #error
-    othererr = err[1:3]
-    othererr[1:2] = [err.drop('T').mean(axis=0)]
-    othererr.index = ['T','O']
-    
-    zoneplot(otherstats,othererr)
-    """
-   
-    
-    
-    
-    gfpmice = gfpmice[['Quadrant time (%)','Unnamed: 29','Unnamed: 30','Unnamed: 31']].astype(float)
-    appmice = appmice[['Quadrant time (%)','Unnamed: 29','Unnamed: 30','Unnamed: 31']].astype(float)
-    
-
-        
-    
-    #error
-    gfperr = gfpmice.apply(scipy.stats.sem, axis=0)
-    gfperr.name = "GFP"
-    apperr = appmice.apply(scipy.stats.sem, axis=0)
-    apperr.name = "APP"
-    err = pd.concat([gfperr,apperr], axis=1)
-    err.index = ['R','T','L','O']
-    #mean
-    avggfpzone = gfpmice.mean(axis=0)
-    avgappzone = appmice.mean(axis=0)
-    avggfpzone.name = "GFP"
-    avgappzone.name = "APP"
-    zonestats = pd.concat([avggfpzone, avgappzone], axis=1)
-    zonestats.index = ['R','T','L','O']
-    
-    quadplot(zonestats,err)
-
-
-
-    
-    
-
-4
+    zone(probe)
+    #quadrant(probe)
+ 
+  
